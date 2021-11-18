608ad747929ad16f9b68ed7dcaf498953dc25ee7
artwaw <artwaw@users.noreply.github.com>
2021-11-18 16:58:48 +0000

    UI prototype ready

--- PAP_en_GB.ts
+++ PAP_en_GB.ts
@@ -1,3 +1,112 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_GB"></TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>Player</name>
+    <message>
+        <location filename="Player.ui" line="14"/>
+        <source>Player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="38"/>
+        <source>Library view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="60"/>
+        <source>Folders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="104"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="111"/>
+        <source>by artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="116"/>
+        <source>by album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="121"/>
+        <source>by genre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="126"/>
+        <source>by year</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="131"/>
+        <source>by artist/album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="139"/>
+        <source>(searched text)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="160"/>
+        <source>Live view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="383"/>
+        <source>Default order</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="388"/>
+        <source>Repeat playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="393"/>
+        <source>Play one track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="398"/>
+        <source>Repeat track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="403"/>
+        <source>Shuffle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="408"/>
+        <source>Shuffle repeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="418"/>
+        <source>Artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="425"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="446"/>
+        <source>00:00:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="204"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

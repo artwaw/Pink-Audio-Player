f10ebd78f8c1808e08e9aad1f869a81e53416702
artwaw <artwaw@users.noreply.github.com>
2021-11-23 09:41:44 +0000

    Updating translations.

--- PAP_en_GB.ts
+++ PAP_en_GB.ts
@@ -12,106 +12,461 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="290"/>
+=======
+        <location filename="Player.ui" line="23"/>
+        <source>Volume:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="82"/>
+>>>>>>> Stashed changes
         <source>Library view</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="312"/>
+=======
+        <location filename="Player.ui" line="104"/>
+>>>>>>> Stashed changes
         <source>Folders</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="356"/>
+=======
+        <location filename="Player.ui" line="148"/>
+>>>>>>> Stashed changes
         <source>View</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="363"/>
+=======
+        <location filename="Player.ui" line="155"/>
+>>>>>>> Stashed changes
         <source>by artist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="368"/>
+=======
+        <location filename="Player.ui" line="160"/>
+>>>>>>> Stashed changes
         <source>by album</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="373"/>
+=======
+        <location filename="Player.ui" line="165"/>
+>>>>>>> Stashed changes
         <source>by genre</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="378"/>
+=======
+        <location filename="Player.ui" line="170"/>
+>>>>>>> Stashed changes
         <source>by year</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="383"/>
+=======
+        <location filename="Player.ui" line="175"/>
+>>>>>>> Stashed changes
         <source>by artist/album</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="391"/>
+=======
+        <location filename="Player.ui" line="183"/>
+>>>>>>> Stashed changes
         <source>(searched text)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="412"/>
         <source>Live view</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="Player.ui" line="191"/>
+=======
+        <location filename="Player.ui" line="451"/>
+>>>>>>> Stashed changes
         <source>Default order</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="196"/>
+=======
+        <location filename="Player.ui" line="456"/>
+>>>>>>> Stashed changes
         <source>Repeat playlist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="201"/>
+=======
+        <location filename="Player.ui" line="461"/>
+>>>>>>> Stashed changes
         <source>Play one track</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="206"/>
         <location filename="Player.cpp" line="117"/>
+=======
+        <location filename="Player.ui" line="466"/>
+        <location filename="Player.cpp" line="118"/>
+>>>>>>> Stashed changes
         <source>Repeat track</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="211"/>
         <location filename="Player.cpp" line="120"/>
+=======
+        <location filename="Player.ui" line="471"/>
+        <location filename="Player.cpp" line="121"/>
+>>>>>>> Stashed changes
         <source>Shuffle</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="216"/>
         <location filename="Player.cpp" line="123"/>
+=======
+        <location filename="Player.ui" line="476"/>
+        <location filename="Player.cpp" line="124"/>
+>>>>>>> Stashed changes
         <source>Shuffle repeat</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="226"/>
+=======
+        <location filename="Player.ui" line="486"/>
+>>>>>>> Stashed changes
         <source>Artist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="233"/>
+=======
+        <location filename="Player.ui" line="493"/>
+>>>>>>> Stashed changes
         <source>Title</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="254"/>
+=======
+        <location filename="Player.ui" line="514"/>
+>>>>>>> Stashed changes
         <source>00:00:00</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="456"/>
         <source>Tab 2</source>
+=======
+        <location filename="Player.cpp" line="31"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="37"/>
+        <source>&amp;Add file...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="41"/>
+        <source>Add &amp;folder...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="45"/>
+        <source>Add &amp;location...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="50"/>
+        <source>New &amp;playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="54"/>
+        <source>Op&amp;en playlist...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="58"/>
+        <source>S&amp;ave playlist...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="63"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="66"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="67"/>
+        <source>&amp;Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="73"/>
+        <source>Select &amp;all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="79"/>
+        <source>&amp;Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="84"/>
+        <source>Remove d&amp;uplicates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="87"/>
+        <source>Remove dead items</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="91"/>
+        <source>&amp;Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="96"/>
+        <source>&amp;Playback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="97"/>
+        <source>&amp;Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="100"/>
+        <source>Pre&amp;v</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="103"/>
+        <source>&amp;Play/Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="107"/>
+        <source>Playback order</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="194"/>
+        <source>Critical error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="194"/>
+        <source>Default playlist can&apos;t be retrieved. This is a bug. Please contact the developer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="204"/>
+        <location filename="Player.cpp" line="109"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="112"/>
+        <source>Repeat all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="115"/>
+        <source>Play single track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="129"/>
+        <source>Stop after current</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="133"/>
+        <source>Cursor follows playback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="136"/>
+        <source>Playback follows cursor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="141"/>
+        <source>&amp;Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="142"/>
+        <source>Clear library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="145"/>
+        <source>Rebuild library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="148"/>
+        <source>Update library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="151"/>
+        <source>La&amp;nguages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="166"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="167"/>
+        <source>&amp;About...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="170"/>
+        <source>&amp;Index</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistManager</name>
+    <message>
+        <location filename="PlaylistManager.cpp" line="23"/>
+        <source>playlists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="39"/>
+        <location filename="PlaylistManager.cpp" line="42"/>
+        <source>New playlist %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="117"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="118"/>
+        <source>Artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="119"/>
+        <source>Genre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="120"/>
+        <source>Copyright</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="121"/>
+        <source>Album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="122"/>
+        <source>Track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="123"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="124"/>
+        <source>Rating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="125"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="126"/>
+        <source>Setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="127"/>
+        <source>Url</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="128"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="129"/>
+        <source>Publisher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="130"/>
+        <source>Encoded by</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="131"/>
+        <source>Artwork url</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PlaylistManager.cpp" line="132"/>
+        <source>Track ID</source>
+>>>>>>> Stashed changes
         <translation type="unfinished"></translation>
     </message>
     <message>

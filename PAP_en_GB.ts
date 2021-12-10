eed388c8a0977c930beee079075b1b35ce38bc7b
artwaw <artwaw@users.noreply.github.com>
2021-12-10 14:39:23 +0000

    Update PAP_en_GB.ts

--- PAP_en_GB.ts
+++ PAP_en_GB.ts
@@ -99,6 +99,7 @@
     </message>
     <message>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="412"/>
         <source>Live view</source>
         <translation type="unfinished"></translation>
@@ -108,28 +109,46 @@
 =======
         <location filename="Player.ui" line="451"/>
 >>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="204"/>
+        <location filename="Player.cpp" line="115"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.ui" line="454"/>
+>>>>>>> Stashed changes
         <source>Default order</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="196"/>
 =======
         <location filename="Player.ui" line="456"/>
+>>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="459"/>
 >>>>>>> Stashed changes
         <source>Repeat playlist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="201"/>
 =======
         <location filename="Player.ui" line="461"/>
+>>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="464"/>
 >>>>>>> Stashed changes
         <source>Play one track</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
         <location filename="Player.ui" line="206"/>
         <location filename="Player.cpp" line="117"/>
@@ -137,10 +156,15 @@
         <location filename="Player.ui" line="466"/>
         <location filename="Player.cpp" line="118"/>
 >>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="469"/>
+        <location filename="Player.cpp" line="127"/>
+>>>>>>> Stashed changes
         <source>Repeat track</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
         <location filename="Player.ui" line="211"/>
         <location filename="Player.cpp" line="120"/>
@@ -159,137 +183,233 @@
         <location filename="Player.ui" line="476"/>
         <location filename="Player.cpp" line="124"/>
 >>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="474"/>
+        <location filename="Player.cpp" line="131"/>
+>>>>>>> Stashed changes
         <source>Shuffle repeat</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="226"/>
 =======
         <location filename="Player.ui" line="486"/>
+>>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="484"/>
 >>>>>>> Stashed changes
         <source>Artist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="233"/>
 =======
         <location filename="Player.ui" line="493"/>
+>>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="491"/>
 >>>>>>> Stashed changes
         <source>Title</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="254"/>
 =======
         <location filename="Player.ui" line="514"/>
+>>>>>>> Stashed changes
+=======
+        <location filename="Player.ui" line="512"/>
 >>>>>>> Stashed changes
         <source>00:00:00</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <location filename="Player.ui" line="456"/>
         <source>Tab 2</source>
 =======
         <location filename="Player.cpp" line="31"/>
+=======
+        <location filename="Player.cpp" line="37"/>
+>>>>>>> Stashed changes
         <source>&amp;File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="37"/>
+=======
+        <location filename="Player.cpp" line="43"/>
+>>>>>>> Stashed changes
         <source>&amp;Add file...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="41"/>
+=======
+        <location filename="Player.cpp" line="47"/>
+>>>>>>> Stashed changes
         <source>Add &amp;folder...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="45"/>
+=======
+        <location filename="Player.cpp" line="51"/>
+>>>>>>> Stashed changes
         <source>Add &amp;location...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="50"/>
+=======
+        <location filename="Player.cpp" line="56"/>
+>>>>>>> Stashed changes
         <source>New &amp;playlist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="54"/>
+=======
+        <location filename="Player.cpp" line="60"/>
+>>>>>>> Stashed changes
         <source>Op&amp;en playlist...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="58"/>
+=======
+        <location filename="Player.cpp" line="64"/>
+>>>>>>> Stashed changes
         <source>S&amp;ave playlist...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="63"/>
+=======
+        <location filename="Player.cpp" line="69"/>
+>>>>>>> Stashed changes
         <source>&amp;Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="66"/>
+=======
+        <location filename="Player.cpp" line="72"/>
+>>>>>>> Stashed changes
         <source>&amp;Edit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="67"/>
+=======
+        <location filename="Player.cpp" line="73"/>
+>>>>>>> Stashed changes
         <source>&amp;Clear</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="73"/>
+=======
+        <location filename="Player.cpp" line="77"/>
+>>>>>>> Stashed changes
         <source>Select &amp;all</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="79"/>
+=======
+        <location filename="Player.cpp" line="81"/>
+>>>>>>> Stashed changes
         <source>&amp;Search...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="84"/>
+=======
+        <location filename="Player.cpp" line="86"/>
+>>>>>>> Stashed changes
         <source>Remove d&amp;uplicates</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="87"/>
+=======
+        <location filename="Player.cpp" line="89"/>
+>>>>>>> Stashed changes
         <source>Remove dead items</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="91"/>
+=======
+        <location filename="Player.cpp" line="93"/>
+>>>>>>> Stashed changes
         <source>&amp;Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="96"/>
+=======
+        <location filename="Player.cpp" line="99"/>
+>>>>>>> Stashed changes
         <source>&amp;Playback</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="97"/>
+=======
+        <location filename="Player.cpp" line="100"/>
+>>>>>>> Stashed changes
         <source>&amp;Next</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="100"/>
+=======
+        <location filename="Player.cpp" line="104"/>
+>>>>>>> Stashed changes
         <source>Pre&amp;v</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="103"/>
+=======
+        <location filename="Player.cpp" line="108"/>
+>>>>>>> Stashed changes
         <source>&amp;Play/Pause</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="107"/>
+=======
+        <location filename="Player.cpp" line="113"/>
+>>>>>>> Stashed changes
         <source>Playback order</source>
         <translation type="unfinished"></translation>
     </message>
@@ -315,62 +435,110 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="115"/>
+=======
+        <location filename="Player.cpp" line="123"/>
+>>>>>>> Stashed changes
         <source>Play single track</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="129"/>
+=======
+        <location filename="Player.cpp" line="137"/>
+>>>>>>> Stashed changes
         <source>Stop after current</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="133"/>
+=======
+        <location filename="Player.cpp" line="141"/>
+>>>>>>> Stashed changes
         <source>Cursor follows playback</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="136"/>
+=======
+        <location filename="Player.cpp" line="144"/>
+>>>>>>> Stashed changes
         <source>Playback follows cursor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="141"/>
+=======
+        <location filename="Player.cpp" line="149"/>
+>>>>>>> Stashed changes
         <source>&amp;Library</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="142"/>
+=======
+        <location filename="Player.cpp" line="150"/>
+>>>>>>> Stashed changes
         <source>Clear library</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="145"/>
+=======
+        <location filename="Player.cpp" line="153"/>
+>>>>>>> Stashed changes
         <source>Rebuild library</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="148"/>
+=======
+        <location filename="Player.cpp" line="156"/>
+>>>>>>> Stashed changes
         <source>Update library</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="151"/>
+=======
+        <location filename="Player.cpp" line="159"/>
+>>>>>>> Stashed changes
         <source>La&amp;nguages</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="166"/>
+=======
+        <location filename="Player.cpp" line="174"/>
+>>>>>>> Stashed changes
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="167"/>
+=======
+        <location filename="Player.cpp" line="175"/>
+>>>>>>> Stashed changes
         <source>&amp;About...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="Player.cpp" line="170"/>
+=======
+        <location filename="Player.cpp" line="178"/>
+>>>>>>> Stashed changes
         <source>&amp;Index</source>
         <translation type="unfinished"></translation>
     </message>
@@ -378,6 +546,7 @@
 <context>
     <name>PlaylistManager</name>
     <message>
+<<<<<<< Updated upstream
         <location filename="PlaylistManager.cpp" line="23"/>
         <source>playlists</source>
         <translation type="unfinished"></translation>
@@ -431,6 +600,87 @@
     <message>
         <location filename="PlaylistManager.cpp" line="125"/>
         <source>Date</source>
+=======
+        <location filename="Player.cpp" line="565"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="627"/>
+        <source>Close active playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="633"/>
+        <source>Rename active playlist</source>
+        <comment>Option to rename playlist tab</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="682"/>
+        <source>Play this</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="689"/>
+        <source>Edit single track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="695"/>
+        <source>Edit selected tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="702"/>
+        <source>Remove track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="708"/>
+        <source>Remove selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="208"/>
+        <source>Critical error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="208"/>
+        <source>Default playlist can&apos;t be retrieved. This is a bug. Please contact the developer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="294"/>
+        <source>New playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="464"/>
+        <source>File(s) to add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="473"/>
+        <source>Add folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="491"/>
+        <source>Playlist name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="491"/>
+        <location filename="Player.cpp" line="614"/>
+        <source>New playlist name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Player.cpp" line="614"/>
+        <source>Rename this playlist</source>
+>>>>>>> Stashed changes
         <translation type="unfinished"></translation>
     </message>
     <message>
